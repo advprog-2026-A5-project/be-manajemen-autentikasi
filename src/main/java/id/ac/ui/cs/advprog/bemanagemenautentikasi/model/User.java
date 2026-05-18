182e662b7eac9c7933dc6dcbc3b6329d4816773b
Kallln <ahaikaln@gmail.com>
2026-05-18 09:20:49 +0700

    [GREEN] implement User model and AuthController for authentication and registration endpoints

--- src/main/java/id/ac/ui/cs/advprog/bemanagemenautentikasi/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/bemanagemenautentikasi/model/User.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.bemanagemenautentikasi.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -9,10 +10,30 @@ import lombok.*;
 @Entity
 @Table(name = "users")
 public class User {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    
+    @Column(unique = true, nullable = false)
     private String username;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String nama;
+    
+    @Column(nullable = false)
     private String password;
+    
+    @Column(nullable = false)
+    private String role; // "ADMIN_UTAMA", "BURUH", "MANDOR", "SUPIR"
+    
+    @Column(name = "nomor_sertifikasi_mandor")
+    private String nomorSertifikasiMandor; // Hanya wajib diisi jika role MANDOR
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mandor_id")
+    private User mandor; // Digunakan saat Admin Utama menugaskan (Assign) Buruh ke Mandor
 }
